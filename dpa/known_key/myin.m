ce22c74d8b0077e55be2d9a69410aa5e9c8776be
GaPhil <gaphil.io@gmail.com>
2018-03-05 11:28:12 +0100

    Refactor

--- dpa/known_key/myin.m
+++
@@ -1,9 +0,0 @@
-function[inputs] = myin(fname, ilen, n)
-myfile = fopen(fname, 'r');
-inputs = zeros(n, ilen);
-for i = 1 : n
-    s = fgets(myfile, 1024);
-    [ii, l] = sscanf(s, '%02x ', ilen);
-    inputs(i, : ) = ii;
-end
-fclose(myfile);
